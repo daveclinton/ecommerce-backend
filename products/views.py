2673c5969682d49a48df6c6d56399e913e566f2f
daveclinton <clintondavid46@gmail.com>
2023-04-19 20:34:59 +0300

    Feat: Add route /

--- products/views.py
+++ products/views.py
@@ -6,14 +6,14 @@ from .products import products
 
 def getRoutes(request):
     routes = [
-        '/api/products',
-        '/api/products/create',
-        '/api/products/upload',
-        '/api/products/<id>/reviews',
-        '/api/products/top',
-        '/api/products/<id>',
-        '/api/products/delete/<id>',
-        '/api/products/<update>/<id>'
+        '/api/products/',
+        '/api/products/create/',
+        '/api/products/upload/',
+        '/api/products/<id>/reviews/',
+        '/api/products/top/',
+        '/api/products/<id>/',
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>/'
     ]
     return JsonResponse(routes, safe=False)
 
